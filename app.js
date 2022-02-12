1457d2f93b9e855e9c90eb492f30dea300521a82
jorge-armando-navarro-flores <jorge.armando.navarro.flores@gmail.com>
2022-02-12 12:40:00 -0600

    post-city

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
+
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get("/", (req, res) => {
-  const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=ead5d26ff3891f8f89fe2da6f9e1fba1&units=metric"
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", (req, res) => {
+  console.log(req.body.cityName);
+
+  const query = req.body.cityName;
+  const apiKey = "ead5d26ff3891f8f89fe2da6f9e1fba1";
+  const unit = "metric";
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + unit;
   https.get(url, (response) => {
     console.log(response.statusCode);
 
@@ -15,13 +28,15 @@ app.get("/", (req, res) => {
       const icon = weatherData.weather[0].icon
       const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
       res.write("<p>The weather is currently " + weatherDescription + "</p>")
-      res.write("<h1>The temperature in London is " + temp + " degrees Celcius.</h1>")
+      res.write("<h1>The temperature in " + query + " is " + temp + " degrees Celcius.</h1>")
       res.write("<img src=" + imageURL + ">")
 
       res.send()
     })
   })
-})
+});
+
+
 
 
 
